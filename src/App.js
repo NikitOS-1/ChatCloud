f28fabcf3a3215c1cfe34346e26b888a8065bca5
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-11-14 13:32:49 +0200

    add depent Lodash and theme with settings

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import Header from "./container/Header/Header";
 import Main from "./container/Main/Main";
 import Footer from "./container/Footer/Footer";
+import {
+  CssBaseline,
+  StyledEngineProvider,
+  ThemeProvider,
+} from "@mui/material";
+import { appTheme } from "./theme";
 
 function App() {
+  const appsTheme = appTheme();
+
   return (
-    <>
-      <Header />
-      <Main />
-      <Footer />
-    </>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={appsTheme}>
+        <CssBaseline />
+        <Header />
+        <Main />
+        <Footer />
+      </ThemeProvider>
+    </StyledEngineProvider>
   );
 }
 
