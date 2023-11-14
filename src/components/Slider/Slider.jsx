5bf4089c9d000def74c0c1b0ec5c40f3ae48b04e
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-11-14 17:34:36 +0200

    connect swiper

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,3 +1,15 @@
+import React, { useRef, useState } from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import "./styles.css";
+
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper/modules";
+
 const MainIllustrationFirst = "/static/Illustaration/MainIllustrationFirst.svg";
 const MainIllustrationSecond =
   "/static/Illustaration/MainIllustrationSecond.svg";
@@ -5,11 +17,32 @@ const MainIllustrationThird = "/static/Illustaration/MainIllustrationThird.svg";
 
 const Slider = () => {
   return (
-    <div>
-      <img src={MainIllustrationFirst} alt="picture" />
-      <img src={MainIllustrationSecond} alt="picture" />
-      <img src={MainIllustrationThird} alt="picture" />
-    </div>
+    <>
+      <Swiper
+        cssMode={true}
+        navigation={true}
+        pagination={true}
+        mousewheel={true}
+        keyboard={true}
+        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+        className="mySwiper">
+        <SwiperSlide>
+          <div>
+            <img src={MainIllustrationFirst} alt="picture" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <img src={MainIllustrationSecond} alt="picture" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <img src={MainIllustrationThird} alt="picture" />
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </>
   );
 };
 export default Slider;
