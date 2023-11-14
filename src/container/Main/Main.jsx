98a33e80b7b0779a7f7671a40b6026049dcf785d
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-11-14 13:34:30 +0200

    ref return Main

--- src/container/Main/Main.jsx
+++ src/container/Main/Main.jsx
@@ -4,7 +4,11 @@ import routes from "../../routes";
 const Main = () => {
   const allPages = useRoutes(routes);
 
-  return <main>main</main>;
+  return (
+    <main>
+      <Container maxWidth="lg">{allPages}</Container>
+    </main>
+  );
 };
 
 export default Main;
