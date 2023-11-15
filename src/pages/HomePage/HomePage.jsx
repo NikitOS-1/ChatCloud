16377cb28db3c712148cbf8a8aae2b0f38f8bc5a
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-11-15 21:26:15 +0200

    separating styles from code

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,67 +1,20 @@
 import Form from "components/Form/Form";
 import Slider from "components/Slider/Slider";
-import styled from "styled-components";
-
-import { theme } from "theme/theme";
-
-const ContainerHomePage = styled.div` max-width: $ {
-  theme.breakpoints.xxl
-}
-
-;
-margin: 0 auto;
-display: flex;
-
-max-height: $ {
-  theme.breakpoints.height
-}
-
-;
-
-@media screen and (max-width: $ {
-    theme.breakpoints.lg
-  }
-
-) {
-  flex-direction: column;
-}
-
-`;
-const ContainerSlider = styled.div` max-width: 761px;
-
-@media screen and (max-width: $ {
-    theme.breakpoints.xl
-  }
-
-) {
-  max-width: 661px;
-}
-
-`;
-const ContainerForm = styled.div` width: 679px;
-
-@media screen and (max-width: $ {
-    theme.breakpoints.xl
-  }
-
-) {
-  max-width: 579px;
-}
-
-`;
+import {
+  ContainerForm,
+  ContainerHomePage,
+  ContainerSlider,
+} from "./styledComponentsHomePage";
 
 const HomePage = () => {
   return (
     <ContainerHomePage>
-      {" "}
       <ContainerSlider>
-        {" "}
-        <Slider />{" "}
-      </ContainerSlider>{" "}
+        <Slider />
+      </ContainerSlider>
       <ContainerForm>
-        {" "}
-        <Form />{" "}
-      </ContainerForm>{" "}
+        <Form />
+      </ContainerForm>
     </ContainerHomePage>
   );
 };
