08a522fce23638b9ab6794a807cc2e10deb90003
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-11-16 21:47:43 +0200

    add category list

--- src/components/Form/CategotyChat/CategoryChat.jsx
+++ src/components/Form/CategotyChat/CategoryChat.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { theme } from "theme/theme";
 const arrowLeft = "/static/other/arrowLeft.svg";
+
 const CategoryChatContainer = styled.div`
   width: 0px;
   padding: 0px;
@@ -22,6 +23,7 @@ const CategoryChatContainer = styled.div`
 const Navigations = styled.div`
   display: flex;
   justify-content: space-between;
+  margin-bottom: 20px;
 `;
 const ButtonArrowLeft = styled.div`
   width: 25px;
@@ -58,10 +60,68 @@ const Category = styled.div`
   flex-direction: column;
 `;
 const CategoryContainer = styled.div`
+  margin-top: 100px;
   width: 100%;
+  height: 350px;
+  display: flex;
+  flex-direction: column;
 `;
 const CategoryItem = styled.div``;
-const Footer = styled.div``;
+const Footer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+const PaginationPage = styled.div`
+  margin-top: 140px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 22px;
+
+  div {
+    border-radius: 50%;
+    width: 8px;
+    height: 8px;
+    border: none;
+
+    background-color: ${theme.colors.neutralColors[200]};
+  }
+
+  :last-child {
+    background-color: ${theme.colors.neutralColors[300]};
+  }
+`;
+const ButtonContinue = styled.button`
+  margin-top: 50px;
+  border-radius: 13px;
+  border: none;
+  width: 230px;
+  height: 55px;
+  color: white;
+  background-color: ${(props) =>
+    !props.$errors
+      ? theme.colors.primary.primaryYellow
+      : theme.colors.neutralColors[300]};
+  font-size: ${theme.fontSizes.Medium20};
+
+  &:hover {
+    cursor: pointer;
+    background-color: ${(props) =>
+      !props.$errors
+        ? theme.colors.hover.yellow
+        : theme.colors.neutralColors[300]};
+  }
+  &:focus-visible {
+    outline: none;
+  }
+`;
+const PrivacyPolicyandTerms = styled.div`
+  margin-top: 60px;
+  span {
+    color: ${theme.colors.primary.primaryYellow};
+  }
+`;
 
 const CategoryChat = ({ next, nextButton }) => {
   let classes = `container ${next ? "visible" : ""}`;
@@ -80,7 +140,19 @@ const CategoryChat = ({ next, nextButton }) => {
         <p>Pick up your favourite topics to set up your feeds</p>
         <CategoryContainer></CategoryContainer>
       </Category>
-      <Footer></Footer>
+      <Footer>
+        <PaginationPage>
+          <div></div>
+          <div></div>
+        </PaginationPage>
+        <ButtonContinue>Continue</ButtonContinue>
+        <PrivacyPolicyandTerms>
+          <p>
+            By clicking "Continue" you agree to our <span>Privacy Policy</span>{" "}
+            and <span>Terms of Service</span>
+          </p>
+        </PrivacyPolicyandTerms>
+      </Footer>
     </CategoryChatContainer>
   );
 };
