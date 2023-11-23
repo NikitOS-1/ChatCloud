40f38e07f39224b2a6e49e64be835025ea4034b5
Lili Str <lilya.strdzk@gmail.com>
2023-11-23 20:21:28 +0200

    feat(component): create layout

---
+++ src/components/Layout/styled.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const StyledLayout = styled.div`
+  height: 100vh;
+  width: 100vw;
+  position: relative;
+  background-color: ${({ theme }) => theme.colors.neutralColors.bgWhite};
+`;
--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { ThemeProvider } from 'styled-components'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ThemeProvider } from 'styled-components';
 
-import App from './App.tsx'
+import App from './App.tsx';
+import { theme } from './theme';
 
-import './index.css'
+import './index.css';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     {/* @todo: pass theme when it is defined */}
-    <ThemeProvider theme={{}}>
+    <ThemeProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>,
-)
+);
