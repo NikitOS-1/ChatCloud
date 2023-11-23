35153cba233a99ebdebc9cbe60ebb7e56c3bc029
apostoliuk97 <53813396+apostoliuk97@users.noreply.github.com>
2023-11-23 19:24:15 +0200

    feat(config): add providers and rules for imports order (#7)
    Co-authored-by: Lili Str <lilya.strdzk@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ThemeProvider } from 'styled-components'
+
 import App from './App.tsx'
+
 import './index.css'
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {/* @todo: pass theme when it is defined */}
+    <ThemeProvider theme={{}}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 )
