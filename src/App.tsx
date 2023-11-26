af1b76df11b853d020b392f7cc7681b50d0e0c2c
Lili Str <lilya.strdzk@gmail.com>
2023-11-26 13:34:01 +0200

    feat(component): add new button, icon component

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,5 @@
-import Button from './components/Button/Button';
-import { StyledLayout } from './components/Layout';
-
 function App() {
-  return (
-    <StyledLayout>
-      <p>container</p>
-      <Button>Button</Button>
-      <p>disabled</p>
-      <Button isDisabled>Button</Button>
-      <p>loading</p>
-      <Button isLoading>Button</Button>
-      <p>icon</p>
-      <Button endIcon={<img src="./" />}>Button</Button>
-      <Button startIcon={<img src="./" />}>Button</Button>
-      <p>outlined</p>
-      <Button variant="outlined">Button</Button>
-      <p>outlined disabled</p>
-      <Button variant="outlined" isDisabled>
-        Button
-      </Button>
-      <p>outlined loading</p>
-      <Button variant="outlined" isLoading>
-        Button
-      </Button>
-      <p>outlined with icon</p>
-      <Button variant="outlined" endIcon={<img src="./" />}>
-        Button
-      </Button>
-      <Button variant="outlined" startIcon={<img src="./" />}>
-        Button
-      </Button>
-    </StyledLayout>
-  );
+  return <div>App</div>;
 }
 
 export default App;
--- src/theme/styled.d.ts
+++ src/theme/styled.d.ts
@@ -74,3 +74,12 @@ declare module 'styled-components' {
     };
   }
 }
+
+declare module '*.svg' {
+  import React = require('react');
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement>
+  >;
+  const src: string;
+  export default src;
+}
