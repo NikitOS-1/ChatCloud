74ee4f9aad4d3e99c9d2a2964c5175ba3493c668
apostoliuk97 <53813396+apostoliuk97@users.noreply.github.com>
2023-12-07 22:45:20 +0200

    feat(component): create input component (#20)
    Co-authored-by: Lili Str <lilya.strdzk@gmail.com>

---
+++ src/components/Input/styled.ts
@@ -0,0 +1,45 @@
+import TextField from '@mui/material/TextField';
+import styled from 'styled-components';
+
+export const TextFieldStyled = styled(TextField)`
+  .MuiFormLabel-root {
+    font-family: ${({ theme }) => theme.fonts.CeraProMedium};
+    font-size: ${({ theme }) => theme.fontSizes.Medium20};
+    color: ${({ theme }) => theme.colors.neutralColors[300]};
+
+    &.Mui-focused {
+      color: ${({ theme }) => theme.colors.primary.primaryBlue};
+    }
+    &.Mui-error {
+      color: ${({ theme }) => theme.colors.supporting.error};
+    }
+  }
+
+  .MuiOutlinedInput-root.MuiInputBase-root {
+    width: 495px;
+    height: 57px;
+    font-family: ${({ theme }) => theme.fonts.CeraProMedium};
+    font-size: ${({ theme }) => theme.fontSizes.Medium20};
+
+    fieldset {
+      border: 2px solid ${({ theme }) => theme.colors.primary.blue30};
+      border-radius: ${({ theme }) => theme.radius.input};
+    }
+
+    &:hover fieldset {
+      border-color: ${({ theme }) => theme.colors.primary.blue50};
+    }
+
+    &.Mui-focused fieldset {
+      border-color: ${({ theme }) => theme.colors.primary.primaryBlue};
+    }
+
+    &.Mui-error fieldset {
+      border-color: ${({ theme }) => theme.colors.supporting.error};
+    }
+
+    &.Mui-disabled fieldset {
+      border-color: ${({ theme }) => theme.colors.neutralColors[200]};
+    }
+  }
+`;
