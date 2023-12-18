9c9a8c6a3033dee63d1bfa1e973067282ecc062b
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-12-18 23:36:26 +0200

    refactor(component): deleted other

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,7 @@
-import { useState } from 'react';
-
 import { StyledLayout } from './components/Layout';
-import { Select } from './components/Select';
-import { BoxStyled } from './components/Select/styled';
-
-interface OptionInterface {
-  label: string;
-  value: {
-    code: string;
-    SKU: number;
-  };
-}
-
-const options: OptionInterface[] = [
-  {
-    label: 'Ukraine',
-    value: {
-      code: 'UA',
-      SKU: 1,
-    },
-  },
-  {
-    label: 'Italy',
-    value: {
-      code: 'IT',
-      SKU: 2,
-    },
-  },
-  {
-    label: 'Germany',
-    value: {
-      code: 'GM',
-      SKU: 3,
-    },
-  },
-];
 
 function App() {
-  const [state, setState] = useState<null | string>(null);
-  console.log(state);
-
-  return (
-    <StyledLayout style={{ margin: '10px' }}>
-      <Select
-        label="Choose a country"
-        options={options}
-        value={state}
-        onChange={(_, option) => {
-          if (option !== null) {
-            setState(
-              typeof option !== 'string' && 'value' in option
-                ? option.label
-                : null,
-            );
-          }
-        }}
-        isOptionEqualToValue={(option) => option.label === state}
-        renderOption={(props, option) => (
-          <BoxStyled component="li" {...props}>
-            <img
-              width="32"
-              src={`https://flagcdn.com/${option.value.code.toLowerCase()}.svg`}
-              alt={option.label}
-            />
-            {option.label}
-          </BoxStyled>
-        )}
-      />
-    </StyledLayout>
-  );
+  return <StyledLayout>App</StyledLayout>;
 }
 
 export default App;
