07865c3bde40421e361caf243765687544610776
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-12-19 17:59:04 +0200

    fix(git): rebase

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,19 +1,19 @@
-import React from "react";
+import React from 'react';
 
-import CircleIcon from "../../assets/icons/circle-icon.svg?react";
-import DeleteIcon from "../../assets/icons/delete-icon.svg?react";
-import ExpandMore from "../../assets/icons/expand-more.svg?react";
-import ReplyIcon from "../../assets/icons/reply-icon.svg?react";
+import CircleIcon from '../../assets/icons/circle-icon.svg?react';
+import DeleteIcon from '../../assets/icons/delete-icon.svg?react';
+import ExpandMore from '../../assets/icons/expand-more.svg?react';
+import ReplyIcon from '../../assets/icons/reply-icon.svg?react';
 
 interface IconProps {
-  name: "delete" | "reply" | "expandMore" | "circle";
+  name: 'delete' | 'reply' | 'expandMore' | 'circle';
   fill?: string;
 }
 
 export const Icon = (props: IconProps) => {
-  const { name, fill = "currentColor", ...svgProps } = props;
+  const { name, fill = 'currentColor', ...svgProps } = props;
 
-  const Icons: Record<IconProps["name"], React.ReactElement> = {
+  const Icons: Record<IconProps['name'], React.ReactElement> = {
     delete: <DeleteIcon fill={fill} {...svgProps} />,
     reply: <ReplyIcon fill={fill} {...svgProps} />,
     expandMore: <ExpandMore fill={fill} {...svgProps} />,
