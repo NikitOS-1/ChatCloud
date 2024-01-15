de113e9938f488ab613d78d6d95b19d3f6eec057
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2024-01-15 18:05:51 +0200

    chore(app): mui icon

--- src/Pages/ChatPage/ChatPage.tsx
+++ src/Pages/ChatPage/ChatPage.tsx
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
+// import ChatOutlinedIcon from "@mui/icons-material/ChatOutlined";
+// import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+// import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
 
 const ChatPageStyled = styled.div``;
-
 export const ChatPage = () => {
   return <ChatPageStyled></ChatPageStyled>;
 };
